f1faaaec32e3da6329cea4104d8968e6b20bf56d
Cam <f-rog@hotmail.com>
2023-03-18 09:18:21 -0400

    Delete Razer_Mouse_Linux-master directory

--- Razer_Mouse_Linux-master/src/nagaUninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-sudo naga stop
-sleep 0.3 > /dev/null 2>&1 &
-echo "Deleting files"
-sudo rm -f /usr/local/bin/naga
-sudo rm -f /usr/local/bin/Naga_Linux/nagaXinputStart.sh
-sudo rm -f /etc/udev/rules.d/80-naga.rules
-sudo rm -f /usr/local/bin/Naga_Linux/nagaKillroot.sh
-sudo rm -f /usr/local/bin/Naga_Linux/nagaUninstall.sh
