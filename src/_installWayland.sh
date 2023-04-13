4594ef9f398759a31a309076af91e822cea6edcf
Cam <f-rog@hotmail.com>
2023-04-13 05:22:07 -0400

    Add files via upload

--- src/_installWayland.sh
+++ src/_installWayland.sh
@@ -36,8 +36,8 @@ cd ..
 rm  -f dotool.tar.gz  > /dev/null
 
 
-cp -rf ./src/window-calls-extended@hseliger.eu ~/.local/share/gnome-shell/extensions
-gnome-extensions enable window-calls-extended@hseliger.eu
+cp -rf ./src/windowTitlesWayland@lostallmymoney.rev ~/.local/share/gnome-shell/extensions
+gnome-extensions enable windowTitlesWayland@lostallmymoney.rev
 
 _dir="/home/$USER/.naga"
 mkdir -p "$_dir"
